07eb1757b670ca54bcc4b5d96c6a88b34e382c82
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-05-23 16:07:34 +0900

    Added genesis.json

--- ethereum.py
+++ ethereum.py
@@ -2,10 +2,11 @@ import time
 from geth import LiveGethProcess # mainnet
 from geth import LoggingMixin, DevGethProcess #private local chain for testing
 from web3 import Web3, EthereumTesterProvider
+from solc import *
 from eth_tester import EthereumTester
 
 geth = DevGethProcess('data1', '/Users/skywalker721/Desktop/go-ethereum/data1') # 해당 파일 경로를 넣어줍니다.
-geth.start()
+print(geth.start())
 
 #w3 = Web3(EthereumTesterProvider())
 #test = EthereumTester()
