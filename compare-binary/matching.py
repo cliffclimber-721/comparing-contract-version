b75e31e03843114991e7058231910d2459f81f55
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-06-02 05:27:44 +0900

    function add

--- compare-binary/matching.py
+++ compare-binary/matching.py
@@ -1,21 +1,24 @@
 from difflib import SequenceMatcher
 
-firstbytecode = input("first input: ")
-secondbytecode = input("second input: ")
+firstbytecode = input("first input : ")
+secondbytecode = input("second input : ")
 first = list(firstbytecode)
 second = list(secondbytecode)
+
+def match_binary():
+    for i in first:
+        print("i:", i)
+    for j in second:
+        print("j:", j)
+    if i == j:
+        pass
+    else:
+        print(i, j)
 
 print("first input length :", len(firstbytecode)) # 정확도를 대비해 길이 확인
 print("second input length :", len(secondbytecode))
 
 showRatio = SequenceMatcher(None, firstbytecode, secondbytecode).ratio()
 
-for first in second:
-    for i in range(0, first):
-        if first == second[i]:
-            print(first)
-            i+=1
-            print(i)
-
-
+match_binary()
 print("show accuracy:", showRatio)
