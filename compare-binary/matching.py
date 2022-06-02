c6f77f4a7f9ad462e0e08ce0ec929ed0b77f2eaf
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-06-02 21:44:45 +0900

    second retouch

--- compare-binary/matching.py
+++ compare-binary/matching.py
@@ -1,19 +1,24 @@
 from difflib import SequenceMatcher
 
+print("================Input=================")
 firstbytecode = input("first input : ")
 secondbytecode = input("second input : ")
+print("================Length================")
 first = list(firstbytecode)
 second = list(secondbytecode)
 
 def match_binary():
-    for i in first:
-        print("i:", i)
-    for j in second:
-        print("j:", j)
-    if i == j:
+    print("======================================")
+    for idx1, value1 in enumerate(first):
+        print("idx1", idx1, "value1", value1)
+    print("======================================")
+    for idx2, value2 in enumerate(second):
+        print("idx2", idx2, "value2", value2)
+    if value1 == value2:
         pass
     else:
-        print(i, j)
+        print("in first bytecode", value1)
+        print("in second bytecode", value2)
 
 print("first input length :", len(firstbytecode)) # 정확도를 대비해 길이 확인
 print("second input length :", len(secondbytecode))
@@ -21,4 +26,5 @@ print("second input length :", len(secondbytecode))
 showRatio = SequenceMatcher(None, firstbytecode, secondbytecode).ratio()
 
 match_binary()
+print("================Accuracy==============")
 print("show accuracy:", showRatio)
